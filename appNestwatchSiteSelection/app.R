8a1d2451fcb57fd24bb46d2dd104242f63cf83c9
Brian S Evans <bsevans.unc@gmail.com>
2015-12-10 16:15:55 -0500

    added raw data table component

--- appNestwatchSiteSelection/app.R
+++ appNestwatchSiteSelection/app.R
@@ -23,35 +23,61 @@ ui <- fluidPage(
               'Pittsburgh, PA' = 'pittsburgh',
               'Raleigh, NC' = 'raleigh',
               'Springfield, MA' = 'springfield',
-              'Washington, D.C' = 'dc')),
+              'Washington, DC' = 'dc')),
       # Data upload, exhisting sites:
         h4(strong('2. Upload existing site data:')),
-          p('Important! The input file must be a three-column csv file of exiting Neighborhood Nestwatch sites with the fields "siteID","longitude" and "latitude" provided in that order.'),
+          p('Important! The input file must be a three-column csv file of exiting 
+            Neighborhood Nestwatch sites with the fields "siteID","longitude" and 
+            "latitude" provided in that order.'),
           fileInput('existingSites','',
             accept = c('test/csv', 'text/comma-separated-values,text/plain','.csv')),
       # Data upload, potential sites:
         h4(strong('3. Upload data for prospective sites:')),
-          p('Important! The input file must be a three-column csv file of exiting Neighborhood Nestwatch sites with the fields "siteID","longitude" and "latitude" provided in that order.'),
+          p('Important! The input file must be a three-column csv file of exiting 
+            Neighborhood Nestwatch sites with the fields "siteID","longitude" and
+            "latitude" provided in that order.'),
           fileInput('potentialSites','',
             accept = c('test/csv', 'text/comma-separated-values,text/plain','.csv')),
       # Define resolution Resolution:
         h4(strong('4. Provide the target resolution:')),
-          p('This is the distance in meters from each household center for which you will calculate the proportion of impervious surface'),
+          p('This is the distance in meters from each household center for which 
+            you will calculate the proportion of impervious surface'),
           numericInput('radius','',500, 50, 5000, step = 50),
       # Submit data for analysis:
-        h4(strong('5. Get summary data!')),
-        submitButton('Submit!'),
-        br(),
-      # Download output:
-        h4(strong('6. Download output')),
-          p('Use the button below if you would like to download a csv file of the proportion of impervious surface surrounding existing and potential sites.'),
-          downloadButton('downloadTabular', 'Download Tabular Output')),
+        h4(strong('5. Select your sites!')),
+        submitButton('Submit!')),
+#         br(),
+#       # Download output:
+#         h4(strong('6. Download output')),
+#           p('Use the button below if you would like to download a csv file of the 
+#             proportion of impervious surface surrounding existing and potential sites.'),
+#           downloadButton('downloadTabular', 'Download Tabular Output')),
       # Graphical summary:
-      mainPanel('Output'))
+      mainPanel(
+        tabsetPanel(
+          tabPanel('Map Viewer', leafletOutput('map'),
+                   ),
+          tabPanel('Summary Plots',plotOutput('plot')),
+          tabPanel('Data Table', tableOutput('table'))
+          )))
 )
 
 # Server
-server <- function(input, output) {}
+server <- function(input, output) {
+  output$table <- renderTable({
+    inFileExistingSites <- input$existingSites
+    inFilePotentialSites <- input$potentialSites
+    if (is.null(inFileExistingSites))
+      return(NULL)
+    existingSites <- read.csv(inFileExistingSites$datapath) %>%
+      mutate(siteType = 'existing', imp = 'FILL')
+    if (is.null(inFilePotentialSites))
+      return(existingSites)
+    potentialSites  <- read.csv(inFilePotentialSites$datapath) %>%
+      mutate(siteType = 'potential', imp = 'FILL')
+    rbind(existingSites, potentialSites)
+  })
+}
 
 shinyApp(ui = ui, server = server)
             
