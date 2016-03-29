e9bcbd8e3d806dc8271d3273e8aac32e73bc630b
Brian S Evans <bsevans.unc@gmail.com>
2016-03-29 10:46:26 -0400

    helper functions for Pc table

--- appNestwatchCRUD/helperFunctions.R
+++ appNestwatchCRUD/helperFunctions.R
@@ -43,7 +43,7 @@ castData <- function(data) {
                       bander = data["bander"],
                       encounterType = data["encounterType"],
                       species = data["species"],
-                      bandNumber = as.integer(data["bandNumber"]),
+                      bandNumber = data["bandNumber"],
                       colorCombo  = data["colorCombo"],
                       age = data["age"],
                       sex = data["sex"],
@@ -189,13 +189,13 @@ getTableMetadata <- function() {
 
 castDataPc <- function(data) {
   datar <- data.frame(sitePc = as.character(data["sitePc"]),
-                      datePc = dateOutPc, #as.character(data["datev"]), #change?
-                      bander = data["bander"],
-                      encounterType = data["encounterType"],
-                      species = data["species"],
-                      bandNumber = as.integer(data["bandNumber"]),
-                      colorCombo  = data["colorCombo"],
-                      notes = as.character(data["notes"]),
+                      observerPc = data["observerPc"],
+                      datePc = dateOutPc, 
+                      timePc = data["timePc"],
+                      speciesPc = data["speciesPc"],
+                      distancePc = data["distancePc"],
+                      countPc  = data["countPc"],
+                      detectionPc = data(data["detectionPc"]),
                       stringsAsFactors = FALSE)
   rownames(datar) <- data["id"]
   return (datar)
@@ -204,57 +204,37 @@ castDataPc <- function(data) {
 # This creates an empty record, to be used e.g. to fill the inputs with the default values when the user clicks the "New" button:
 
 createDefaultRecordPc <- function() {
-  mydefault <- castData(list(id = "0", 
-                             sitev = '',
-                             datev = Sys.time(),
-                             bandTime = '',
-                             bander = '',
-                             encounterType = '',
-                             species = '',
-                             bandNumber = '',
-                             colorCombo  = '',
-                             age = '',
-                             sex = '',
-                             breedingCond = '',
-                             fat = '',
-                             mass = '',
-                             wing = '',
-                             tail = '',
-                             tarsus = '',
-                             featherID = '',
-                             notes = ''))
+  mydefault <- castData(list(idPc = "0", 
+                             sitePc = '',
+                             observerPc = '',
+                             datePc = Sys.time(),
+                             timePc = '',
+                             speciesPc = '',
+                             distancePc = '',
+                             countPc  = '',
+                             detectionPc = ''))
   return (mydefault)
 }
 
 # And this method takes the data as selected in the DataTable, and updates the inputs with the respective values:
 
 updateInputsPc <- function(data, session) {
-  updateTextInput(session, "id", value = unname(rownames(data)))
-  updateTextInput(session, "sitev", value = unname(data['sitev']))
-  updateTextInput(session, "datev", value = dateOut)#unname(as.character(data['datev'])))
-  updateTextInput(session, "bandTime", value = unname(data["bandTime"]))
-  updateTextInput(session, "bander", value = unname(data["bander"]))
-  updateTextInput(session, "encounterType", value = unname(data["encounterType"]))
-  updateTextInput(session, "species", value = unname(data["species"]))
-  updateTextInput(session, "bandNumber", value = unname(data["bandNumber"]))
-  updateTextInput(session, "colorCombo", value = unname(data["colorCombo"]))
-  updateTextInput(session, "age", value = unname(data["age"]))
-  updateTextInput(session, "sex", value = unname(data["sex"]))
-  updateTextInput(session, "breedingCond", value = unname(data["breedingCond"]))
-  updateTextInput(session, "fat", value = unname(data["fat"]))
-  updateTextInput(session, "mass", value = unname(data["mass"]))
-  updateTextInput(session, "wing", value = unname(data["wing"]))
-  updateTextInput(session, "tail", value = unname(data["tail"]))
-  updateTextInput(session, "tarsus", value = unname(data["tarsus"]))
-  updateTextInput(session, "featherID", value = unname(data["featherID"]))
-  updateTextInput(session, "notes", value = unname(data["notes"]))
+  updateTextInput(session, "idPc", value = unname(rownames(data)))
+  updateTextInput(session, "sitePc", value = unname(data['sitePc']))
+  updateTextInput(session, "observerPc", value = unname(data['observerPc']))
+  updateTextInput(session, "datePc", value = dateOutPc)#unname(as.character(data['datev'])))
+  updateTextInput(session, "timePc", value = unname(data["timePc"]))
+  updateTextInput(session, "speciesPc", value = unname(data["speciesPc"]))
+  updateTextInput(session, "distancePc", value = unname(data["distancePc"]))
+  updateTextInput(session, "countPc", value = unname(data["countPc"]))
+  updateTextInput(session, "detectionPc", value = unname(data["detectionPc"]))
 }
 
 # This function finds the next ID of a new record. In mysql, this could be done by an incremental index, automatically. But here, we do it manually, ourselves:
 
 getNextIdPc <- function() {
-  if (exists("responses")) {
-    max(as.integer(rownames(responses))) + 1
+  if (exists("responsesPc")) {
+    max(as.integer(rownames(responsesPc))) + 1
   } else {
     return (1)
   }
@@ -265,8 +245,8 @@ getNextIdPc <- function() {
 createDataPc <- function(data) {
   data <- castData(data)
   rownames(data) <- getNextId()
-  if (exists("responses")) {
-    responses <<- rbind(responses, data)
+  if (exists("responsesPc")) {
+    responses <<- rbind(responsesPc, data)
   } else {
     responses <<- data
   }
@@ -275,8 +255,8 @@ createDataPc <- function(data) {
 # Read
 
 readDataPc <- function() {
-  if (exists("responses")) {
-    responses
+  if (exists("responsesPc")) {
+    responsesPc
   }
 }
 
@@ -284,38 +264,28 @@ readDataPc <- function() {
 
 updateDataPc <- function(data) {
   data <- castData(data)
-  responses[row.names(responses) == row.names(data), ] <<- data
+  responsesPc[row.names(responsesPc) == row.names(data), ] <<- data
 }
 
 
 # Delete
 
 deleteDataPc <- function(data) {
-  responses <<- responses[row.names(responses) != unname(data["id"]), ]
+  responsesPc<<- responses[row.names(responsesPc) != unname(data["idPc"]), ]
 }
 
 # The only thing that might not be straight forward is the GetTableMetadata function. We'll use it as a starting point for further development, as described below. For now, it's just a method that defines the names of the columns in our table:
 
 getTableMetadataPc <- function() {
-  fields <- c(id = "Id",
-              sitev = "Site",
-              datev = "Date",
-              bandTime = 'Time',
-              bander = 'Observer',
-              encounterType = 'Encounter type',
-              species = 'SPP',
-              bandNumber = 'Band number',
-              colorCombo  = 'Color combo',
-              age = 'Age',
-              sex = 'Sex',
-              breedingCond = 'CP/BP',
-              fat = 'Fat',
-              mass = 'Mass',
-              wing = 'Wing',
-              tail = 'Tail',
-              tarsus = 'Tarsus',
-              featherID = 'Feather ID',
-              notes = "Notes")
+  fields <- c(idPc = "Id",
+              sitePc = "Site",
+              observerPc = 'Observer',
+              datePc = "Date",
+              timePc = 'Time',
+              speciesPc = 'SPP',
+              distancePc = 'Distance',
+              countPc  = 'Count',
+              detectionPc = 'Detection')
   result <- list(fields = fields)
   return (result)
 }
