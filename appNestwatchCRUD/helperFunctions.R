1f000fef01d066971d75de3ab0df9fd563955e7e
Brian S Evans <bsevans.unc@gmail.com>
2016-03-29 11:29:21 -0400

    added savePcData

--- appNestwatchCRUD/helperFunctions.R
+++ appNestwatchCRUD/helperFunctions.R
@@ -30,6 +30,17 @@ saveEncounterData <- function(encounterData) {
   drop_upload(filePath, dest = 'encounterData')
 }
 
+savePcData <- function(pcData) {
+  data <- pcData
+  # Create a unique file name
+  fileName <- sprintf("%s_%s.csv", as.integer(Sys.time()), digest::digest(data))
+  # Write the data to a temporary file locally
+  filePath <- file.path(tempdir(), fileName)
+  write.csv(data, filePath, row.names = FALSE, quote = TRUE)
+  # Upload the file to Dropbox
+  drop_upload(filePath, dest = 'pcData')
+}
+
 #---------------------------------------------------------------------------------*
 # ---- The complicated path from input to editable data frame ----
 #=================================================================================*
