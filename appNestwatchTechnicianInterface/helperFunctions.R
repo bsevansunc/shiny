4198f0f6bc4fcdcb7d6d0ca885a9ebe3ffa062a4
Brian S Evans <bsevans.unc@gmail.com>
2016-04-22 08:01:49 -0400

    removed commented out portions

--- appNestwatchTechnicianInterface/helperFunctions.R
+++ appNestwatchTechnicianInterface/helperFunctions.R
@@ -36,60 +36,9 @@ saveData <- function(data, dataName, siteName){
   drop_upload(filePath, dest = destinationFolder)
 }
 
-# Save visit data to Dropbox:
-# 
-# saveVisitData <- function(visitData) {
-#   # data <- visitData 
-#   # Create a unique file name
-#   fileName <- sprintf("%s_%s.csv", as.integer(Sys.time()), digest::digest(data)) #! This may be a problem!
-#   # Write the data to a temporary file locally
-#   filePath <- file.path(tempdir(), fileName)
-#   write.csv(visitData, filePath, row.names = FALSE, quote = TRUE)
-#   # Upload the file to Dropbox
-#   drop_upload(filePath, dest = 'nnDataStorage/visitData')
-# }
-
-# Save encounter data to Dropbox:
-
-# saveEncounterData <- function(encounterData) {
-#   data <- encounterData
-#   # Create a unique file name
-#   fileName <- sprintf("%s_%s.csv", as.integer(Sys.time()), digest::digest(data))
-#   # Write the data to a temporary file locally
-#   filePath <- file.path(tempdir(), fileName)
-#   write.csv(data, filePath, row.names = FALSE, quote = TRUE)
-#   # Upload the file to Dropbox
-#   drop_upload(filePath, dest = 'nnDataStorage/encounterData')
-# }
-
-# Save point count data (counts) to Dropbox:
-
-# savePcDataCounts <- function(pcData) {
-#   data <- pcData
-#   # Create a unique file name
-#   fileName <- sprintf("%s_%s.csv", as.integer(Sys.time()), digest::digest(data))
-#   # Write the data to a temporary file locally
-#   filePath <- file.path(tempdir(), fileName)
-#   write.csv(data, filePath, row.names = FALSE, quote = TRUE)
-#   # Upload the file to Dropbox
-#   drop_upload(filePath, dest = 'pcDataCounts')
-# }
-
 #---------------------------------------------------------------------------------*
 # ---- Create, Read, Update, Delete ----
 #=================================================================================*
-
-# Existence check
-
-# existCheck <- function(dataObjectToCheck){
-#   deparse(substitute(dataObjectToCheck)) %>% exists
-# }
-
-# Assign an object to the global environment (more flexible than <<- as it allows the user to specify the name of the object inside the function)
-# 
-# globalAssign <- function(data, dataName){
-#   assign(dataName, data, envir = .GlobalEnv )
-# }
 
 # Function assigns fancy column headers to the field codes:
 
@@ -107,25 +56,6 @@ castData <- function(fieldValues){
   return(data.frame(data, stringsAsFactors = FALSE))
 }
 
-# Cast a blank record:
-
-# createDefaultRecord <- function(fieldCodes){
-#   # Empty list for field code storage:
-#   defaultFieldValues <- vector('list', length = length(fieldCodes))
-#   for(i in 1:length(fieldCodes)){
-#     # If the field is already defined, provide definition:
-#     if(exists(fieldCodes[i])){
-#       defaultFieldValues[[i]] <- get(fieldCodes[i])
-#       # If the field is undefined, set default as blank:
-#     } else {
-#       defaultFieldValues[[i]] <- ''
-#     }
-#   }
-#   # Provide object names and cast data:
-#   names(defaultFieldValues) <- fieldCodes
-#   castData(defaultFieldValues)
-# }
-
 # Make some submissions blank:
 
 createBlankInputs <- function(fieldCodes, session){
@@ -138,23 +68,11 @@ createBlankInputs <- function(fieldCodes, session){
 
 updateInputs <- function(data, fieldCodes, session) {
   for(i in 1:length(fieldCodes)){
-    updateTextInput(session, fieldCodes[i],
-                    value = unname(data[fieldCodes[i]]))
+    updateTextInput(
+      session, fieldCodes[i],
+      value = unname(data[fieldCodes[i]])
+      )
   }
 }
 
-# Update vector of select inputs based on input value:
-# 
-# updateSelectInputVector <- function(selectInputVector, input, session){
-#   siteInputs <- c('siteEnc', 'siteQuery', 'sitePc', 'siteNest')
-#   for(i in 1:length(selectInputVector)){
-#     updateSelectInput(session, 
-#                       selectInputVector[i],
-#                       selected = input[[selectInputVector[i]]])
-#   }
-# }
-
-# Update input selections:
-
-# updateSelectedInputs <- function(inputsToUpdate, )
 
